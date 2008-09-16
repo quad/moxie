28bcb9f2d5f83590287d797364462e7dce8ba6da
Scott Robinson <scott@quadhome.com>
2008-09-15 23:35:00 -0700

    Setup rearchitected.

--- setup.py
+++ setup.py
@@ -8,13 +8,20 @@ setup(
 
     packages = find_packages(),
     package_data = {
-        '': ['templates/*', 'static/*'],
+        'moxie': ['templates/*', 'static/*'],
     },
 
     install_requires = [
-        'web.py >= 0.22',
-        'mutagen >= 1.11',
-        'markdown',
+        'flup >= 1.0',
+        'Mako >= 0.2.2',
+        'Markdown >= 1.6',
+        'mutagen >= 1.14',
+        'Routes >= 1.9.2',
+        'WebOb >= 0.9.3',
+    ],
+    dependency_links = [
+        # mutagen
+        "http://code.google.com/p/quodlibet/downloads/list",
     ],
 
     entry_points = {
