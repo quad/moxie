6a45e90191ba18eb3a9572b70df6a87c722858e3
Scott Robinson <scott@quadhome.com>
2008-09-16 22:35:20 -0700

    Refactor the music tests to advertise scons heavily.

--- tests/music-tests.py
+++ tests/music-tests.py
@@ -61,8 +61,8 @@ class TrackListHeaderTest(unittest.TestCase):
         assert tracks.header == self.header
         assert len(tracks) == 0
 
-class TrackInfoTests(unittest.TestCase):
-    """TrackInfo tests."""
+class TrackInfoNegativeTests(unittest.TestCase):
+    """TrackInfo tests on non-existent data."""
 
     @raises(IOError)
     def test_not_found(self):
@@ -71,22 +71,38 @@ class TrackInfoTests(unittest.TestCase):
         fn = tempfile.mktemp()
         TrackInfo(fn)
 
+    @raises(IOError)
+    def test_invalid_directory(self):
+        """A 'mp3' that is a directory."""
+
+        TrackInfo(DATA)
+
+class TrackInfoDataTests(unittest.TestCase):
+    """TrackInfo tests on prepared data."""
+
+    NULL_MP3 = os.path.join(DATA, 'null-noid3.mp3')
+    V1_MP3   = os.path.join(DATA, 'null-v1.mp3')
+    V2_MP3   = os.path.join(DATA, 'null-v2.mp3')
+
+    def setUp(self):
+        print "No such file or directory? Run 'scons'."
+
     def test_v1(self):
         """An ID3v1 tagged file."""
 
-        info = TrackInfo(os.path.join(DATA, 'null-v1.mp3'))
+        info = TrackInfo(self.V1_MP3)
         self.check_info(info)
 
     def test_v2(self):
         """An ID3v2 tagged file."""
 
-        info = TrackInfo(os.path.join(DATA, 'null-v2.mp3'))
+        info = TrackInfo(self.V2_MP3)
         self.check_info(info)
 
     def test_none(self):
         """An untagged file."""
 
-        info = TrackInfo(os.path.join(DATA, 'null-noid3.mp3'))
+        info = TrackInfo(self.NULL_MP3)
 
         assert info.album == 'No Album'
         assert info.artist == 'No Artist'
@@ -99,9 +115,3 @@ class TrackInfoTests(unittest.TestCase):
         assert info.artist == 'Null Artist'
         assert info.duration == '0:00'
         assert info.title == 'Null Title'
-
-    @raises(IOError)
-    def test_invalid_directory(self):
-        """A 'mp3' that is a directory."""
-
-        TrackInfo(DATA)
