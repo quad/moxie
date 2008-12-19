343553de111b2cac8db60b99448e5d0b1bc191d9
Scott Robinson <scott@quadhome.com>
2008-12-19 14:14:13 -0800

    Isolate the release script to a virtualenv.

--- release.sh
+++ release.sh
@@ -1,6 +1,15 @@
 #!/bin/sh -x
 
 git clean -df
+
+if [ -d "release-moxie" ];
+	rm -rf release-moxie
+fi
+virtualenv release-moxie
+. release-moxie/bin/activate
+
 scons
+
 ./setup test
+
 ./setup sdist bdist_egg
