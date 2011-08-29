61dda757de35f6e1ba2cce424daffec23a6376e2
Scott Robinson <scott.robinson@thoughtworks.com>
2011-08-29 21:46:18 +1000

    Rewrote to use SoundManager2.

--- moxie/static/moxie.js
+++ moxie/static/moxie.js
@@ -1,129 +1,99 @@
-var STOPPED = 0;
-var PLAYING = 1;
-var PAUSED = 2;
+soundManager.onready(function () {
+	"use strict";
 
-var _player;
-var _player_status;
-var _player_track;
-var _player_position;
+	var original_title = document.title;
 
-var _original_title;
-
-var _songs;
-
-function render_duration(time) {
-	var seconds = time % 60;
-	var minutes = Math.floor(time / 60) % 60;
-	var hours   = Math.floor(time / (60 * 60));
-
-	function leadingZero(s) {
-		return (s > 9) ? s : ("0" + s);
-	}
-
-	var duration = ":" + leadingZero(seconds);
-
-	if (hours) {
-		duration = hours + ":" + leadingZero(minutes) + duration;
-	}
-	else if (minutes) {
-		duration = minutes + duration;
-	}
-
-	return duration;
-}
-
-function track_update() {
-	_songs.each(function (index) {
-		var message = $(this).find("span.position");
-
-		if (_player_status && _player_track == index) {
-			var counter;
-
-			// Update the position counter.
-
-			if (_player_position) {
-				var time = Math.round(_player_position / 1000);
-
-				counter = render_duration(time);
-			}
-			else {
-				counter = "&hellip;";
-			}
-
-			counter = counter + "&nbsp;/&nbsp;";
-
-			message.html(counter);
-
-			// Update the CSS styles.
-
-			$(this).addClass("active");
-
-			if (_player_status == PLAYING) {
-				$(this).addClass("playing");
-				$(this).removeClass("paused");
-
-				// Update the title with the track name.
-				var title_dom = $(this).find(".title");
-				document.title = $.trim(title_dom.text()) + " | " + _original_title;
-			}
-			else if (_player_status == PAUSED) {
-				$(this).addClass("paused");
-				$(this).removeClass("playing");
-			}
+	function render_duration(time) {
+		function leadingZero(s) {
+			return (s > 9) ? s : ("0" + s);
 		}
-		else {
-			// Clear any stale position counters.
-			message.html("");
 
-			// Clear any stale CSS styles.
-			$(this).removeClass("active");
-			$(this).removeClass("playing");
-			$(this).removeClass("paused");
+		var seconds  = time % 60,
+		    minutes  = Math.floor(time / 60) % 60,
+		    hours    = Math.floor(time / (60 * 60)),
+		    duration = ":" + leadingZero(seconds);
+
+		if (hours) {
+			duration = hours + ":" + leadingZero(minutes) + duration;
+		} else if (minutes) {
+			duration = minutes + duration;
 		}
+
+		return duration;
+	}
+
+	$("li.song").each(function () {
+		var e       = $(this),
+		    next_e  = e.next(),
+		    message = function (html) { e.find(".position").html(html); },
+		    timer   = function (html) { message(html + "&nbsp;/&nbsp;"); };
+
+		e.data("sound", soundManager.createSound({
+			id: e.attr("id"),
+			url: e.find("a").attr("href"),
+			whileloading: function () {
+				e.addClass("active");
+
+				if (!this.position) {
+					timer("&hellip;");
+				}
+			},
+			whileplaying: function () {
+				e.addClass("active");
+
+				if (this.position) {
+					var time = Math.round(this.position / 1000);
+					timer(render_duration(time));
+
+					e.addClass("playing");
+					e.removeClass("paused");
+
+					// Update the title with the track name.
+					document.title = $.trim(e.find(".title").text()) + " | " + original_title;
+				}
+			},
+			onpause: function () {
+				e.addClass("paused");
+				e.removeClass("playing");
+			},
+			onjustbeforefinish: function () {
+				var next_sound = next_e.data("sound");
+
+				if (next_sound) {
+					next_sound.load();
+				}
+			},
+			onstop: function () {
+				message("");
+
+				e.removeClass("active");
+				e.removeClass("paused");
+				e.removeClass("playing");
+
+				document.title = original_title;
+			},
+			onfinish: function () {
+				this.options.onstop();
+
+				var next_sound = next_e.data("sound");
+
+				if (next_sound) {
+					next_sound.play();
+				}
+			}
+		}));
 	});
 
-	if (_player_status == STOPPED) {
-		// Reset the title.
-		document.title = _original_title;
-	}
-}
+	$("li.song").click(function () {
+		var track = $(this).data("sound");
 
-function track_click() {
-	// This is a work-around for IE not supporting .currentTarget.
-	// http://www.quirksmode.org/js/events_order.html
-	var track = parseInt($(this).attr("id"), 10);
-
-	// Pause if we're playing the track, otherwise play it!
-	if (_player_status == PLAYING && _player_track == track) {
-		_player.MusicPlayer_pause();
-	}
-	else {
-		if (_player_track == track) {
-			_player.MusicPlayer_play();
+		if (track.playState) {
+			track.togglePause();
+		} else {
+			soundManager.stopAll();
+			track.play();
 		}
-		else {
-			_player.MusicPlayer_play(track);
-		}
-	}
 
-	// Immediately update.
-	track_update();
-}
-
-function MusicPlayer_callback() {
-	// Cache repeated references.
-	_player = swfobject.getObjectById("player");    // Use swfobject for browser compatibility.
-	_songs = $("li.song");
-	_original_title = document.title;
-
-	// Bind DOM events.
-	_songs.click(track_click);
-}
-
-function MusicPlayer_update(status, track, position) {
-	_player_status = status;
-	_player_track = track;
-	_player_position = position;
-
-	track_update();
-}
+		return false;
+	});
+});
