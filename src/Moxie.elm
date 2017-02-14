840d2a82333d83dd6e1a8f6d47931cdc2337b6bf
Scott Robinson <scott@quadhome.com>
2017-02-14 20:13:47 +0800

    elm-format

--- src/Moxie.elm
+++ src/Moxie.elm
@@ -4,134 +4,181 @@ import Html exposing (a, div, h1, li, text, span, ul, program)
 import Html.Attributes exposing (class, href, id)
 import List exposing (indexedMap)
 
-type Time = Time Float
-type URL = URL String
-type Status = Playing Time | Paused Time | Stopped
+
+type Time
+    = Time Float
+
+
+type URL
+    = URL String
+
+
+type Status
+    = Playing Time
+    | Paused Time
+    | Stopped
+
 
 type alias Model =
-  { header : Header
-  , tracks : List Track
-  }
+    { header : Header
+    , tracks : List Track
+    }
+
 
 type alias Header =
-  { title : String
-  , subtitle : String
-  , url : URL
-  }
+    { title : String
+    , subtitle : String
+    , url : URL
+    }
+
 
 type alias Track =
-  { artist : String
-  , title : String
-  , duration : Time
-  , url : URL
-  , status: Status
-  }
+    { artist : String
+    , title : String
+    , duration : Time
+    , url : URL
+    , status : Status
+    }
 
-type Msg = None
+
+type Msg
+    = None
+
 
 main : Program Never Model Msg
-main = program { init = init, view = view, update = update, subscriptions = subscriptions }
+main =
+    program { init = init, view = view, update = update, subscriptions = subscriptions }
 
-init : (Model, Cmd Msg)
+
+init : ( Model, Cmd Msg )
 init =
-  ( { header =
-      { title = "model.header.title"
-      , subtitle = "model.header.subtitle"
-      , url = URL "#"
+    ( { header =
+            { title = "model.header.title"
+            , subtitle = "model.header.subtitle"
+            , url = URL "#"
+            }
+      , tracks =
+            [ { artist = "track 1"
+              , title = "title 1"
+              , duration = Time 123.4
+              , url = URL "#1"
+              , status = Playing <| Time 12.34
+              }
+            , { artist = "track 2"
+              , title = "title 2"
+              , duration = Time 61.0
+              , url = URL "#2"
+              , status = Paused <| Time 1.234
+              }
+            , { artist = "track 3"
+              , title = "title 3"
+              , duration = Time 0
+              , url = URL "#3"
+              , status = Stopped
+              }
+            ]
       }
-    , tracks =
-      [ { artist = "track 1",
-          title = "title 1",
-          duration = Time 123.4,
-          url = URL "#1",
-          status = Playing <| Time 12.34
-        },
-        { artist = "track 2",
-          title = "title 2",
-          duration = Time 61.0,
-          url = URL "#2",
-          status = Paused <| Time 1.234
-        },
-        { artist = "track 3",
-          title = "title 3",
-          duration = Time 0,
-          url = URL "#3",
-          status = Stopped
-        }
-      ]
-    }
-  , Cmd.none
-  )
+    , Cmd.none
+    )
+
 
 view : Model -> Html.Html Msg
-view {header, tracks} =
-  div []
-    [ header_view header
-    , tracks_view
-        <| indexedMap track_view
-        <| tracks
-    ]
+view { header, tracks } =
+    div []
+        [ header_view header
+        , tracks
+            |> indexedMap track_view
+            |> tracks_view
+        ]
+
 
 header_view : Header -> Html.Html Msg
-header_view {title, subtitle, url} =
-  case url of
-    URL url ->
-      div [id "header"]
-        [ h1 [] [text title]
-        , a [href url] [text subtitle]
-        ]
+header_view { title, subtitle, url } =
+    case url of
+        URL url ->
+            div [ id "header" ]
+                [ h1 [] [ text title ]
+                , a [ href url ] [ text subtitle ]
+                ]
+
 
 tracks_view : List (Html.Html msg) -> Html.Html msg
 tracks_view tracks =
-  ul [class "songs"] tracks
+    ul [ class "songs" ] tracks
+
 
 track_view : Int -> Track -> Html.Html Msg
-track_view index {artist, title, url, duration, status} =
-  let
-    number = toString (index + 1)
-    (URL url_) = url
-    (track_class, time) = case status of
-      Playing d ->
-        ("song playing", d)
-      Paused d ->
-        ("song paused", d)
-      Stopped ->
-        ("song", Time 0.0)
-    track_id = "track_" ++ number
-    track_name = artist ++ " - " ++ title
-    track_time = minutes_and_seconds time
-    track_duration = minutes_and_seconds duration
-  in
-    li
-      [ class track_class
-      , id track_id
-      ]
-      [ a [class "title", href url_] [text track_name]
-      , span [class "time"]
-        [ span [class "position"] [text track_time]
-        , span [class "duration"] [text track_duration]
-        ]
-      ]
+track_view index { artist, title, url, duration, status } =
+    let
+        number =
+            toString (index + 1)
+
+        (URL url_) =
+            url
+
+        ( track_class, time ) =
+            case status of
+                Playing t ->
+                    ( "song playing", t )
+
+                Paused t ->
+                    ( "song paused", t )
+
+                Stopped ->
+                    ( "song", Time 0.0 )
+
+        track_id =
+            "track_" ++ number
+
+        track_name =
+            artist ++ " - " ++ title
+
+        track_time =
+            minutes_and_seconds time
+
+        track_duration =
+            minutes_and_seconds duration
+    in
+        li
+            [ class track_class
+            , id track_id
+            ]
+            [ a [ class "title", href url_ ] [ text track_name ]
+            , span [ class "time" ]
+                [ span [ class "position" ] [ text track_time ]
+                , span [ class "duration" ] [ text track_duration ]
+                ]
+            ]
+
 
 minutes_and_seconds : Time -> String
 minutes_and_seconds (Time time) =
-  let
-    seconds =
-      floor time % 60
-        |> toString
-        |> String.padLeft 2 '0'
-    minutes =
-      floor time // 60
-        |> toString
-  in
-    if time >= 60 then
-      minutes ++ ":" ++ seconds
-    else
-      ":" ++ seconds
+    let
+        t =
+            floor time
 
-update : Msg -> Model -> (Model, Cmd msg)
+        seconds =
+            t
+                % 60
+                |> toString
+                |> String.padLeft 2 '0'
+
+        minutes =
+            t
+                // 60
+                |> toString
+    in
+        if time >= 60 then
+            minutes ++ ":" ++ seconds
+        else
+            ":" ++ seconds
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
-  (model, Cmd.none)
+    ( model, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model =
+    Sub.none
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -5,49 +5,48 @@ import Expect
 import Fuzz exposing (float, floatRange)
 import Random exposing (maxInt)
 import Test exposing (..)
-
 import Moxie exposing (Time, minutes_and_seconds)
 
 
 all : Test
 all =
-  describe "Moxie Test Suite"
-    [ describe "minutes_and_seconds"
-      [ test "0 is :00" <|
-        \() ->
-          Moxie.Time 0
-            |> minutes_and_seconds
-            |> Expect.equal ":00"
-      , test "60 is 1:00" <|
-        \() ->
-          Moxie.Time 60
-            |> minutes_and_seconds
-            |> Expect.equal "1:00"
-      , fuzz float "The seconds part are digits" <|
-        \f ->
-          Moxie.Time f
-            |> minutes_and_seconds
-            |> String.right 2
-            |> String.all isDigit
-            |> Expect.true "One of the seconds' characters wasn't a digit"
-      , fuzz float "The third character is always a ':'" <|
-        \f ->
-          Moxie.Time f
-            |> minutes_and_seconds
-            |> String.slice -3 -2
-            |> Expect.equal ":"
-      , fuzz (floatRange 0 59) ":00 - :59 are three characters long" <|
-        \f ->
-          Moxie.Time f
-            |> minutes_and_seconds
-            |> String.length
-            |> Expect.equal 3
-      , fuzz (floatRange 60 (toFloat Random.maxInt)) "1:00, the minute part is digits" <|
-        \f ->
-          Moxie.Time f
-            |> minutes_and_seconds
-            |> String.dropRight 3
-            |> String.all isDigit
-            |> Expect.true "One of the minutes' characters wasn't a digit"
-      ]
-    ]
+    describe "Moxie Test Suite"
+        [ describe "minutes_and_seconds"
+            [ test "0 is :00" <|
+                \() ->
+                    Moxie.Time 0
+                        |> minutes_and_seconds
+                        |> Expect.equal ":00"
+            , test "60 is 1:00" <|
+                \() ->
+                    Moxie.Time 60
+                        |> minutes_and_seconds
+                        |> Expect.equal "1:00"
+            , fuzz float "The seconds part are digits" <|
+                \f ->
+                    Moxie.Time f
+                        |> minutes_and_seconds
+                        |> String.right 2
+                        |> String.all isDigit
+                        |> Expect.true "One of the seconds' characters wasn't a digit"
+            , fuzz float "The third character is always a ':'" <|
+                \f ->
+                    Moxie.Time f
+                        |> minutes_and_seconds
+                        |> String.slice -3 -2
+                        |> Expect.equal ":"
+            , fuzz (floatRange 0 59) ":00 - :59 are three characters long" <|
+                \f ->
+                    Moxie.Time f
+                        |> minutes_and_seconds
+                        |> String.length
+                        |> Expect.equal 3
+            , fuzz (floatRange 60 (toFloat Random.maxInt)) "1:00, the minute part is digits" <|
+                \f ->
+                    Moxie.Time f
+                        |> minutes_and_seconds
+                        |> String.dropRight 3
+                        |> String.all isDigit
+                        |> Expect.true "One of the minutes' characters wasn't a digit"
+            ]
+        ]
